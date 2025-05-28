9d1ed83155709620965a2fbf758ef8774dcf5d7e
lukasfecko <lucasfecko@gmail.com>
2025-05-28 09:03:50 +0200

    upgrade

--- src/Client/BCSoapClient.php
+++ src/Client/BCSoapClient.php
@@ -161,7 +161,6 @@ class BCSoapClient
 			'Size'     => $f->getSize(),
 			'Format'   => $f->getFormat(),
 			'Mode'     => $f->getUploadMode(),
-			'SkipCheckDuplicates' => false,
 		];
 
 		if ($f->getSeparator() !== null) {
